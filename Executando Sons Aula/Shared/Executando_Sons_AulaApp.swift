190285fa588c1d9d1ced9a45b30b8ba871ea1b2c
Gilberto Silva <gilberto.silva@sigga.com>
2021-04-04 15:38:33 -0300

    Criando o projeto.

---
+++ Executando Sons Aula/Shared/Executando_Sons_AulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executando_Sons_AulaApp.swift
+//  Shared
+//
+//  Created by Gilberto Silva on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Executando_Sons_AulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
